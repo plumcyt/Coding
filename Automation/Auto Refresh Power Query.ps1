b92c23dc4c44bb397baa50f6fd5f22fd7164fef9
plumcyt <70322691+plumcyt@users.noreply.github.com>
2021-12-02 15:12:49 +0800

    Update Auto Refresh Power Query.ps1

--- Automation/Auto Refresh Power Query.ps1
+++ Automation/Auto Refresh Power Query.ps1
@@ -1,1 +1,21 @@
+$Excel = New-Object -COM "Excel.Application"
+$Excel.Visible = $true
+$Workbook = $Excel.Workbooks.Open("C:\Users\username\Desktop\excel.xlsx")
 
+foreach ($sheet in $Workbook.Sheets) {
+    $sheet.QueryTables | ForEach-Object {
+        while ($_.QueryTable.Refreshing) {
+            Start-Sleep -Seconds 1
+        }
+    }
+}
+
+$Excel.Save()
+$Excel.Close()
+
+# clear the COM object you have created after finishing with them to free memory:
+
+$null = [System.Runtime.Interopservices.Marshal]::ReleaseComObject($Workbook)
+$null = [System.Runtime.Interopservices.Marshal]::ReleaseComObject($Excel)
+[System.GC]::Collect()
+[System.GC]::WaitForPendingFinalizers()
